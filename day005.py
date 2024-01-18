0413f3ed41a57722b88893b3f8e625b127304c74
YoungSi4 <dksdudgns01@gmail.com>
2024-01-18 14:24:36 +0900

    day005 - prime map

--- day005.py
+++ day005.py
@@ -51,6 +51,8 @@
 #                 return False
 #             i += 1
 #         return True # True를 반환하고 함수를 즉시 종료한다.
+
+
 # # 이전에 만들어둔 소수 찾는 코드
 #
 # help(isprime)
@@ -157,3 +159,75 @@
 # print(x)
 # print(type(x))
 
+# 익명함수 가기 전에 map. mapping의 줄임말. 사상하다 비추다라는 뜻
+numbers = ["7", "-11", "3"]
+total_sum = 0
+for number in numbers:
+    total_sum += int(number)
+print(total_sum)             # 너무 길고 복잡하다.
+
+numbers = ["7", "-11", "3"]
+print(sum(map(int, numbers))) # map(함수이름, 적용할 대상)
+# 말도 안 되게 짧아졌다.
+
+
+# 소수 탐색에 map 적용해서 개선
+
+def isprime(n) -> bool: # 리턴값 명시. 없어도 되는데 있으면 더 좋다.
+    """
+    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean type으로 반환
+    :param n: 소수인지 판정할 매개변수
+    :return: 소수 True, 아니면 False
+    """
+    pass
+    if n < 2:
+        return False # False를 반환하고 함수를 즉시 종료한다.
+
+    else:
+        i = 2
+        while i*i <= n:
+            if n % i == 0:
+                return False
+            i += 1
+        return True # True를 반환하고 함수를 즉시 종료한다.
+
+while True:
+    menu = input("1) Fahrenheit -> Celsius  2) Celsius -> Fehrenheit 3) Prime number 4) Prime number range 5) Quit Program: ")
+
+    if menu == '1':
+        fahrenheit = float(input('Input Fahenheit : '))
+        print(f'화씨온도: {fahrenheit}F, 섭씨: {((fahrenheit - 32) * 5 / 9):.4f}C')
+
+    elif menu == '2':
+        celsius = float(input('Input Celsius: '))
+        print(f'Celsius: {celsius}C, Fahrenheit: {((celsius * 9/5) + 32):.4f}F')
+
+    elif menu == '3':
+        number = int(input("Input Number : "))
+        if isprime(number):
+            print(f"{number} is prime number")
+        else:
+            print(f"{number} is not prime number")
+
+    elif menu == '4':
+        n1, n2 = map(int, input("Input first second number: ").split()) # 여기를 수정했다.
+        n1, n2 = min(n1, n2), max(n1, n2) # 밑의 if문을 내장 함수로 간소화
+
+        # numbers = input("Input First Second Number : ").split() # 숫자 하나만 입력하는 경우?
+        # n1 = int(numbers[0])
+        # n2 = int(numbers[1])
+
+
+        # if n1 > n2:
+        #     n1, n2 = n2, n1
+
+        for number in range(n1, n2 + 1):
+            if isprime(number):
+                print(number, end=' ')
+        print() # print 자체에 줄바꿈을 가지고 있다
+
+    elif menu == '5':
+        print('Terminate Program. Goodnight.')
+        break
+    else:
+        pass
