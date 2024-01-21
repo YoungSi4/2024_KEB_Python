03efb8bf4a420b217596a79ec7b87084c5c95364
YoungSi4 <dksdudgns01@gmail.com>
2024-01-22 00:35:47 +0900

    Weekend Project - battle

--- Weekend Assignment - Pokemon Project.py
+++ Weekend Assignment - Pokemon Project.py
@@ -1,12 +1,15 @@
 # Pokemon Project
 
+import random
+import sys
+
 # class
 # 최초에 생성할 때 디폴트 네임 + 해당 포켓몬의 타입을 적어줘야함
 class Attack_skill():
-    def __init__(self, name, damage, miss, type):
+    def __init__(self, name, power, accuracy, type):
         self.skill_name = name
-        self.damage = damage
-        self.miss = miss
+        self.power = power
+        self.accuracy = accuracy
         self.type = type
 
 class tackle(Attack_skill):
@@ -26,6 +29,17 @@ class whip_sweep(Attack_skill):
     def __init__(self):
         super().__init__("덩쿨채찍", 40, 100, "풀")
 
+class bubble_beam(Attack_skill):
+    def __init__(self):
+        super().__init__("거품광선", 65, 100, "물")
+
+class flame_wheel(Attack_skill):
+    def __init__(self):
+        super().__init__("화염자동차", 60, 100, "불")
+
+class masiclal_leaves(Attack_skill):
+    def __init__(self):
+        super().__init__("매지컬리프", 60, 100, "풀")
 
 
 class Human():
@@ -49,11 +63,43 @@ class Villain(Human):
     def __init__(self):
         super().__init__("연수", "아직없음")
 
+def type_chart(my_type, target_type, skill_type) -> float: # 타입상성, 자속보정 여부 반환
+    type_adv, type_effect = 1.0, 1.0
+
+    if my_type == skill_type:
+        type_adv = 1.5
+
+    if skill_type == "물":
+        if target_type == "불":
+            type_effect = 2.0
+        elif target_type == "물" or "풀":
+            type_effect = 0.5
+        else:
+            type_effect = 1.0
+
+    if skill_type == "불":
+        if target_type == "풀":
+            type_effect = 2.0
+        elif target_type == "불" or "물":
+            type_effect = 1.0
+        else:
+            type_effect = 0.5
+
+    if skill_type == "풀":
+        if target_type == "물":
+            type_effect = 2.0
+        elif target_type == "풀" or "물":
+            type_effect = 0.5
+        else:
+            type_effect = 1.0
+
+    return type_effect * type_adv
+
 class Pokemon():
     def __init__(self, name, type, hp, atk, defence, skills):
     # text
         self.name = name
-        self.type = (type,)
+        self.type = type
         self.skill = skills
 
     # basic numeric status
@@ -65,7 +111,9 @@ class Pokemon():
     # act
     def attack(self, target, skill):
         print(f'{self}(이)가 {target.name}에게 {skill.name}!')
-        # 데미지 계산 및 적용 스탭 추가할 예정
+        # (데미지 = (위력 × 공격 × (레벨 × [[급소]] × 2 ÷ 5 + 2 ) ÷ 방어 ÷ 50 + 2 ) × [[자속 보정]] × 타입상성1)
+        damage = int((skill.power * self.atk / target.defence / 50 + 2 )* type_chart(self.type, target.type, skill.type))
+        target.current_hp -= damage
 
 
 # # 포켓몬 리스트
@@ -91,9 +139,56 @@ class Gyaradose(Pokemon):   # 체육관 관장 # 물
 
 def pokemon_center():
     pass
+def skill_select(pokemon, selected_num):
+    return pokemon.skill[selected_num - 1]
 
-def battle():
-    pass
+def display_skill(pokemon):
+    str = ""
+    for i in range(0, 4):
+        if pokemon.skill[i] is None:
+            str += f"{i + 1}) [---] "
+            continue
+        str += f"{i + 1}) [{pokemon.skill[i].skill_name}] "
+    print(str, end=' ')
+
+def battle(person1, person2):
+    isBattle = True
+    input(f"{person2}(이)가 승부를 걸어왔다!")
+    input(f"{person2.name}: 가랏! {person2.partner.name}!")
+    input(f"당신은 {person1.partner.name}(을)를 내보냈다!")
+
+    while isBattle == True:
+        select = 0
+        select = int(input(f"무엇을 할까? 1) 싸운다 2) 도망간다"))
+
+        if select == 1: # 1) 싸운다
+            #select_skill = 0
+            #select_skill = int((input(f"1) {person1.partner.skill[0].skill_name} 2) {person1.partner.skill[1].skill_name} 3) {person1.partner.skill[2].skill_name} 4){person1.partner.skill[0].skill_name} 5) 뒤로가기: ")))
+            display_skill(person1.partner)
+            select_skill = int(input("5) 뒤로가기: "))
+            if 4 < select_skill:
+                continue
+            else:
+                selected_skill = skill_select(person1.partner, select_skill) # 기술 선택만 수행
+                person1.partner.attack(person2.partner, selected_skill)
+                if person2.partner.currnet_hp <= 0:
+                    input(f"{person2.partner.name}(이)가 쓰려졌다!")
+                    input(f"당신은 전투에서 승리했다!")
+                    break
+                # 우리 포켓몬 공격 턴 종료 시점(적 공격 시작 시점)
+                person2.partner.attack(person1.partner, random.randint(0, 3))
+                if person1.partner.current_hp <= 0:
+                    input(f"{person1.partner.name}(이)가 쓰러졌다!")
+                    input("당신은 전투에서 패배했다!")
+                    input("눈앞이 깜깜해졌다...")
+                    input("당신은 패배했습니다. 게임을 종료합니다.")
+                    sys.exit(0) # 바로 실행을 종료시키는 명령어
+
+        if select == 2: # 2) 도망친다
+            input("승부 중에 등을 보일 순 없어!")
+            continue
+
+
 
 # 객체 생성
 gyaradose_mac = Gyaradose()
@@ -175,3 +270,7 @@ while True:
 # 선택 이후
 input(f"{rival.name}: 넌 {hero.partner.name}을 골랐구나!")
 input(f"{rival.name}: 그럼 난 {rival.partner.name}으로 해야지!")
+input(f"{rival.name}: 이제 포켓몬도 생겼으니 나랑 승부다!")
+battle(hero, rival)
+input("전투종료 이후진행") # 임시
+
