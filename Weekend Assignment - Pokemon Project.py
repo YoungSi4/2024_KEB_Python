004017c3ff9921121adcc973ecac1da2e8731ecf
YoungSi4 <dksdudgns01@gmail.com>
2024-01-21 15:23:39 +0900

    Weekend Project - select starting

--- Weekend Assignment - Pokemon Project.py
+++ Weekend Assignment - Pokemon Project.py
@@ -94,6 +94,7 @@ def pokemon_center():
 def battle():
     pass
 
+# 객체 생성
 gyaradose_mac = Gyaradose()
 alakazam_yeon = Alakazam()
 hero = Hero()
@@ -104,18 +105,18 @@ villain = Villain()
 # 게임 내용 text
 
 # intro
-input("마박사: 흐음")
-input("마박사: 잘 왔다!")
-input("마박사: 포켓몬스터의 세계에 온 것을 환영한다!")
-input("마박사: 내 이름은 마박사!")
-input("마박사: 모두가 포켓몬 박사님이라 부르고 있단다.")
-input("마박사: 이 세계에는")
-input("마박사: 포켓몬스터")
-input("마박사: 줄여서 '포켓몬'이라 불리는")
-input("마박사: 신기한 생명체가")
-input("마박사: 도처에 살고 있다!")
-input("마박사: 그건 그렇고 이제 슬슬")
-input("마박사: 자네에 대해 알아보도록 하지!")
+input("박사: 흐음")
+input("박사: 잘 왔다!")
+input("박사: 포켓몬스터의 세계에 온 것을 환영한다!")
+input("박사: 내 이름은 마박사!")
+input("박사: 모두가 포켓몬 박사님이라 부르고 있단다.")
+input("박사: 이 세계에는")
+input("박사: 포켓몬스터")
+input("박사: 줄여서 '포켓몬'이라 불리는")
+input("박사: 신기한 생명체가")
+input("박사: 도처에 살고 있다!")
+input("박사: 그건 그렇고 이제 슬슬")
+input("박사: 자네에 대해 알아보도록 하지!")
 hero.name = input("마박사: 자네의 이름은? : ")
 input("흐음...")
 input(f"{hero.name}라고 하는가!")
@@ -127,5 +128,49 @@ input(f"{hero.name}!!")
 input("이제부터 너만의 이야기가 시작된다!")
 input("행운을 빌지!")
 
+# select starting pokemon
+input("쾅!")
+input("???: 약속이 언제인데 아직도 안 나와!")
+input(f"{rival.name}: 늦으면 벌금 500만원이니까!")
+input("쾅!")
+input("당신은 나갈 채비를 한다")
+input(f"{rival.name}: 늦었어!! 빨리 가자!!")
+input(f"당신과 {rival.name}은 박사님의 연구실로 향한다.")
+input("연구실 문을 열고 들어간다.")
+input("박사: 왔는가!")
+input("박사: 자, 이 친구들은 내가 요즘 연구하고 있는 아이들이라네")
+input("박사: 밖은 위험하니 데려가도록 하게나.")
 
+# 스타팅 포켓몬 선택
+while True:
+    Start_select = 0
+    Yes_or_No = None
+    Start_select = int(input("포켓몬을 골라주세요 1) 아차모 2) 팽도리 3) 이상해씨 : "))
+    if Start_select == 1:
+        Yes_or_No = input("'아차모' 이 아이로 하시겠습니까? 1) 네 2) 아니오 : ")
+        if Yes_or_No == 1:
+            hero.partner = Torchic()
+            rival.partner = Bullbasuar()
+            break
+        if Yes_or_No == 0:
+            continue
+    if Start_select == 2:
+        Yes_or_No = input("'팽도리' 이 아이로 하시겠습니까? 1) 네 2) 아니오 : ")
+        if Yes_or_No == 1:
+            hero.partner = Bullbasuar()
+            rival.partner = Torchic()
+            break
+        if Yes_or_No == 0:
+            continue
+    if Start_select == 3:
+        Yes_or_No = input("'이상해씨' 이 아이로 하시겠습니까? 1) 네 2) 아니오 : ")
+        if Yes_or_No == 1:
+            hero.partner = Piplup()
+            rival.partner = Bullbasuar()
+            break
+        if Yes_or_No == 0:
+            continue
 
+# 선택 이후
+input(f"{rival.name}: 넌 {hero.partner.name}을 골랐구나!")
+input(f"{rival.name}: 그럼 난 {rival.partner.name}으로 해야지!")
