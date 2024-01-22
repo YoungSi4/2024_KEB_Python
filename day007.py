f71a1de56b3032b7a99ad91784f0061a91fb46da
YoungSi4 <dksdudgns01@gmail.com>
2024-01-22 11:32:34 +0900

    class day7 - aggregation

--- day007.py
+++ day007.py
@@ -1,18 +1,9 @@
-# # 간단하게 매직 메소드 써보기.
-class FlyMixin:
-    def fly(self):
-        return f'{self.__name}의 공중날기!'
-
-
-class SurfMixin:
-    def Surf(self):
-        return f"{self.__name}의 파도타기!"  # 이건 Mixin 설명을 위해 사용했음
-
 
 class Pokemon:
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, fly):
         self.__name = name  # 진짜 이름
         self.hp = hp
+        self.fly = fly # aggregation (has-a)
 
     def attack(self):
         print("공격")
@@ -35,17 +26,28 @@ class Pokemon:
         # return self.__name + " + " + target.__name
         return f'두 포켓몬의 체력 합은 {self.hp + target.hp}입니다.'
 
+class Fly: # 리스코프 치환의 법칙
+    def fly(self):
+        return f'공중날기!'
 
-class Charizard(Pokemon, FlyMixin):
-    pass
+class JetPack(Fly):
+    def fly(self):
+        return "10분간 날 수 있습니다"
 
+class Nofly(Fly):
+    def fly(self):
+        return f'하늘을 못 날아요'
 
-class Gyarados(Pokemon, SurfMixin, FlyMixin):
-    pass
+class FlyWithWings(Fly):
+    def fly(self):
+        return f"날개로 하늘을 날아갑니다"
 
+class Pikachu:
+    def __init__(self, name, hp, fly):
+        self.name = name
+        self.hp = hp
+        self.fly_behavior = fly
 
-g1 = Gyarados("갸라도스", 100)
-ch1 = Charizard("리자몽", 120)
-print(g1) # __str__ used
-print(ch1)
-print(g1+ch1) # 클래스끼리 더할 수 있나...?
+no_wings = Nofly()
+p1 = Pikachu("피카츄", 100, no_wings)
+print(p1.fly_behavior.fly()) # 이게 aggregation. has-a
